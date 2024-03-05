0f0e7b8417c14510e02b1d3ff3fb5a4728dccc37
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2024-03-04 21:37:54 -0300

    Primeiro commit - criação de components + interpolação de dados

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent implements OnInit {
+name: string = 'Paulo';
+age: number = 30;
+job = 'Cadista';
+hobbies = ['Correr', ' Desenhar', 'Assistir Anime', 'Musculação']
+
+constructor() {};
+ngOnInit(): void {};
+
+}
