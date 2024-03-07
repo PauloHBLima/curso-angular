ddab086e774537919eb9d2a9553c244d2bd4d1e4
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2024-03-06 22:30:37 -0300

    diretivas de estilos, recurso usando estilos reutilizaveis usando propriedades no componente ts e usando no css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { ParentDataSecondComponent } from './components/parent-data-second/parent-data-second.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentDataComponent,
-    ParentDataSecondComponent
+    ParentDataSecondComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
